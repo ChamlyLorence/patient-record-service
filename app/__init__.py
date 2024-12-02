a8b161600a929e3e76764ad9d159b4ea0e3b730c
PF0ZBBZA\Lenovo <Chamly.Lorance@cloudsolutions.com.sa>
2024-12-02 23:45:00 +0530

    locally tested , docker image also tested, and UI part added

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,15 @@ from app.routes import patient_blueprint
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///patients.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///patients.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
     db.init_app(app)
+
+    # Create the tables at startup
+    with app.app_context():
+        db.create_all()
+
     app.register_blueprint(patient_blueprint)
+
     return app
