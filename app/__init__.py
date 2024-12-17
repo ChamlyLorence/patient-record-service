f15e2b45dc19ad9c50a9163fd970b00929858a72
PF0ZBBZA\Lenovo <Chamly.Lorance@cloudsolutions.com.sa>
2024-12-17 18:40:46 +0530

    change the db to postgres

--- app/__init__.py
+++ app/__init__.py
@@ -4,15 +4,16 @@ from app.routes import patient_blueprint
 
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///patients.db"
+    
+    # PostgreSQL database URI
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:postgres@ec2-52-71-203-176.compute-1.amazonaws.com:5432/meditrack"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
 
-    # Create the tables at startup
+    # Ensure tables are created
     with app.app_context():
         db.create_all()
 
     app.register_blueprint(patient_blueprint)
-
     return app
